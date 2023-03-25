7f14137c50d74ea1b9e0489a426569d279625304
LisieAlmeida <91788754+LisieAlmeida@users.noreply.github.com>
2023-03-24 21:12:45 -0300

    Implementando o front da página Ranking

--- src/test/java/com/capgemini/RankingTests.java
+++ src/test/java/com/capgemini/RankingTests.java
@@ -19,47 +19,47 @@ public class RankingTests {
 	@Test
 	public void testUpdateRankingWith11Users() {
 	    Ranking ranking = new Ranking();
-	    User user1 = new User("Alice", "alice@gmail.com", "1234566");
+	    User user1 = new User("Alice Borba", "alice@gmail.com", "1234566");
 	    user1.setPoints(10);
 	    ranking.addUser(user1);
 
-	    User user2 = new User("Bob", "bob@gmail.com", "154878");
+	    User user2 = new User("Roberto Braga", "roberto@gmail.com", "154878");
 	    user2.setPoints(20);
 	    ranking.addUser(user2);
 
-	    User user3 = new User("Charlie", "charlie@gmail.com", "548484");
+	    User user3 = new User("Maria Clara", "maria@gmail.com", "548484");
 	    user3.setPoints(30);
 	    ranking.addUser(user3);
 
-	    User user4 = new User("Dave", "dave@gmail.com", "5648746");
+	    User user4 = new User("Davi Silva", "davi@gmail.com", "5648746");
 	    user4.setPoints(40);
 	    ranking.addUser(user4);
 
-	    User user5 = new User("Eve", "eve@gmail.com", "5544841");
+	    User user5 = new User("Evandro Cardoso", "evandro@gmail.com", "5544841");
 	    user5.setPoints(50);
 	    ranking.addUser(user5);
 
-	    User user6 = new User("Frank", "frank@gmail.com", "6549894");
+	    User user6 = new User("Francisco Santos", "francisco@gmail.com", "6549894");
 	    user6.setPoints(60);
 	    ranking.addUser(user6);
 
-	    User user7 = new User("Grace", "grace@gmail.com", "54878");
+	    User user7 = new User("Graciela Matos", "graciela@gmail.com", "54878");
 	    user7.setPoints(70);
 	    ranking.addUser(user7);
 
-	    User user8 = new User("Harry", "harry@gmail.com", "6966488");
+	    User user8 = new User("Rafael Cordeiro", "rafael@gmail.com", "6966488");
 	    user8.setPoints(80);
 	    ranking.addUser(user8);
 
-	    User user9 = new User("Isabel", "isabel@gmail.com", "6458977");
+	    User user9 = new User("Isabel Rocha", "isabel@gmail.com", "6458977");
 	    user9.setPoints(90);
 	    ranking.addUser(user9);
 
-	    User user10 = new User("Jack", "jack@gmail.com", "56857897");
+	    User user10 = new User("Jaqueline Maria", "jack@gmail.com", "56857897");
 	    user10.setPoints(100);
 	    ranking.addUser(user10);
 
-	    User user11 = new User("Kelly", "kelly@gmail.com", "2659889");
+	    User user11 = new User("Kelly Carvalho", "kelly@gmail.com", "2659889");
 	    user11.setPoints(110);
 	    ranking.addUser(user11);
 
